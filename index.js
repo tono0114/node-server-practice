cf6629551577d18ade969020be64160c938c2088
tono0114 <83708233+tono0114@users.noreply.github.com>
2021-11-25 16:36:09 +0900

    respond Heroku

--- index.js
+++ index.js
@@ -5,18 +5,11 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.get("/", function (req, res) {
-//   console.log(req);
-//   res.send("<h1>トップページ！！</h1>");
-// });
-
 app.post("/api/v1/quiz", function (req, res) {
   const answer = req.body.answer;
   if (answer === "2") {
-    // res.send("<h1>正解！</h1>");
     res.redirect("/correct.html")
   } else {
-    // res.send("<h1>不正解！</h1>");
     res.redirect("/wrong.html")
   }
 });
@@ -28,6 +21,7 @@ app.get("/api/v1/users", function (req, res) {
   });
 });
 
-app.listen(3000, function () {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
   console.log("I am running!");
 });
